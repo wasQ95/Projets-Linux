d42ff9429e5783026ed273bf65c78d0d9a133a91
wasQ95 <45238569+wasQ95@users.noreply.github.com>
2019-05-27 23:15:02 +0200

    Update changeProxy.sh

--- changeProxy.sh
+++ changeProxy.sh
@@ -5,20 +5,18 @@ echo ""
 echo "[1] Changement adresse IP"
 echo "[2] Changement PROXY"
 echo "[3] Changement adresse MAC"
-echo ""
+echo "[4] Configuration Automatique Lycée (proxy,ftp,)"
 
 read choix;
 
 case $choix in 
-1) echo "Sur quelle interface êtes-vous ? (ex : eth0, enp0s3)"
 
-read interface;
+#------------------------------------------------------------------------------------------------------------------------
+1) 
 
-echo "vous avez saisi l'interface $interface )"
+sudo nano /etc/network/interfaces
 ;;
-
-
-
+#------------------------------------------------------------------------------------------------------------------------
 2)
 clear
 echo "Vous avez choisi le changement du PROXY"
@@ -29,4 +27,33 @@ echo "indiquez le port: "
 read portProxy;
 echo "Acquire:http::proxy \"http://$adresseProxy:$portProxy/\";" | sudo tee -a /etc/apt/apt.conf.d/70debconf
 ;;
+#------------------------------------------------------------------------------------------------------------------------
+3)
+
+;;
+#------------------------------------------------------------------------------------------------------------------------
+4)
+clear
+echo "Configuration automatique en cours..."
+
+#APT-CACHE
+echo "Configuration APT-CACHE..."
+echo 'Acquire:http::proxy "http://10.1.0.10:9999/";' | sudo tee -a /etc/apt/apt.conf.d/70debconf
+
+#PROXY HTTP
+echo "Configuration PROXY HTTP..."
+echo 'http_proxy"http://10.1.2.5:8080/";' | sudo tee -a /etc/environment
+echo 'https_proxy"http://10.1.2.5:8080/";' | sudo tee -a /etc/environment
+echo 'ftp_proxy"http://10.1.2.5:8080/";' | sudo tee -a /etc/environment
+
+
+#DELOCK DPKG
+echo "Deblocage DPKG..."
+sudo lsof /var/lib/dpkg/lock/
+
+# REDEMARRAGE CARTE-RESEAU
+sudo service networking restart
+
+;;
 esac
+#------------------------------------------------------------------------------------------------------------------------
